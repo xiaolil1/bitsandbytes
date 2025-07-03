9a3b980f21fb8aa868da234babbcc40193323219
xiaolil1 <xiaoli.liu@intel.com>
2025-07-03 08:52:10 +0000

    Enable draft GEMM/GEMV kernel

--- csrc/xpu_cutlass.h
+++ csrc/xpu_cutlass.h
@@ -2,36 +2,36 @@
 
 #include <float.h>
 
-//#include "cutlass/epilogue/collective/default_epilogue.hpp"
-//#include "cutlass/epilogue/collective/xe_epilogue.hpp"
-//#include "cutlass/epilogue/fusion/xe_callbacks.hpp"
-//#include "cutlass/gemm/device/gemm_universal.h"
-//#include "cutlass/gemm/device/gemm_universal_adapter.h"
-//#include "cutlass/gemm/collective/collective_mma.hpp"
-//#include "cutlass/util/GPU_Clock.hpp"
-//#include "cutlass/epilogue/dispatch_policy.hpp"
-//#include <cute/atom/copy_traits_xe.hpp>
-//
-//#include <cute/tensor.hpp>
-//#include <random>
-//
-//#include "cutlass/util/command_line.h"
-//#include "cutlass/util/device_memory.h"
-//#include "cutlass/util/packed_stride.hpp"
-//#include "cutlass/util/reference/device/gemm_complex.h"
-//#include "cutlass/util/reference/device/tensor_compare.h"
-//#include "sycl_common.hpp"
-//#include "helper.h"
+#include "cutlass/epilogue/collective/default_epilogue.hpp"
+#include "cutlass/epilogue/collective/xe_epilogue.hpp"
+#include "cutlass/epilogue/fusion/xe_callbacks.hpp"
+#include "cutlass/gemm/device/gemm_universal.h"
+#include "cutlass/gemm/device/gemm_universal_adapter.h"
+#include "cutlass/gemm/collective/collective_mma.hpp"
+#include "cutlass/util/GPU_Clock.hpp"
+#include "cutlass/epilogue/dispatch_policy.hpp"
+#include <cute/atom/copy_traits_xe.hpp>
 
-#include "cutlass/cutlass.h"
-#include "cutlass/gemm/dispatch_policy.hpp"
-#include "cutlass/gemm/gemm.h"
-#include "cutlass/kernel_hardware_info.hpp"
+#include <cute/tensor.hpp>
+#include <random>
 
-#include "cute/algorithm/functional.hpp"
-#include "cute/atom/mma_atom.hpp"
-#include "cute/algorithm/gemm.hpp"
-#include "cute/tensor_predicate.hpp"
+#include "cutlass/util/command_line.h"
+#include "cutlass/util/device_memory.h"
+#include "cutlass/util/packed_stride.hpp"
+#include "cutlass/util/reference/device/gemm_complex.h"
+#include "cutlass/util/reference/device/tensor_compare.h"
+#include "sycl_common.hpp"
+#include "helper.h"
+
+//#include "cutlass/cutlass.h"
+//#include "cutlass/gemm/dispatch_policy.hpp"
+//#include "cutlass/gemm/gemm.h"
+//#include "cutlass/kernel_hardware_info.hpp"
+//
+//#include "cute/algorithm/functional.hpp"
+//#include "cute/atom/mma_atom.hpp"
+//#include "cute/algorithm/gemm.hpp"
+//#include "cute/tensor_predicate.hpp"
 
 using namespace cute;
 
